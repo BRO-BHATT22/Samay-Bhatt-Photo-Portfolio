ba7efcf2d342c2c930e306cf5c3adcc00aeca729
BRO-BHATT22 <525sunaybhatt@frhsd.com>
2025-03-28 13:48:31 -0400

    worked more on HTML and navbar.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('.randomize img').each(function() {
     var rando = 1 + Math.floor(Math.random() * 16);
     var url = $(this).attr('src');
-    if (rando == 4 || rando == 6 || rando == 9 || rando == 11 || rando == 16 ) { // Such a total hack. Some of these PlaceKittens images don't return.
+    if (rando == 4 || rando == 6 || rando == 9 || rando == 11 || rando == 16 ) {
       rando = rando - 1;
     }
     if (rando == 7 ) {
